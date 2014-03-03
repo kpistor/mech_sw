d2cfe2cf9941a3a3d74e16e04484318d773feb98
Kyle <ktpistor@gmail.com>
2014-03-03 15:42:00 -0800

    swc_all

---
+++ params.rb
@@ -0,0 +1,25 @@
+# CONFIRMATION_NUMBER = 'ZKWFE6'
+# FIRST_NAME = 'Tyler'
+# LAST_NAME = 'Petersen'
+# FLIGHT_DEPARTURE = '2013-12-23 22:30' #time in format: yyyy-mm-dd hh:mm
+# EMAIL = 'ktpistor@gmail.com'
+
+# CONFIRMATION_NUMBER = 'ZNI65H'
+# FIRST_NAME = 'Kyle'
+# LAST_NAME = 'Pistor'
+# FLIGHT_DEPARTURE = '2013-12-23 22:30' #time in format: yyyy-mm-dd hh:mm
+# EMAIL = 'ktpistor@gmail.com'
+
+
+# CONFIRMATION_NUMBER = 'ZHHZA9'
+# FIRST_NAME = 'Kyle'
+# LAST_NAME = 'Pistor'
+# FLIGHT_DEPARTURE = '2013-12-23 22:30' #time in format: yyyy-mm-dd hh:mm
+# EMAIL = 'ktpistor@gmail.com'
+
+CONFIRMATION_NUMBER = 'ZKBE6J'
+FIRST_NAME = 'Conrad'
+LAST_NAME = 'White'
+FLIGHT_DEPARTURE = '2013-12-23 22:30' #time in format: yyyy-mm-dd hh:mm
+EMAIL = 'vitullimarkus@gmail.com'
+# EMAIL = 'ktpistor@gmail.com'
