2d1eba7ab32c2f5bb3bd4f51422c6f9f2e171a36
kpistor <kpistor@users.noreply.github.com>
2015-07-21 10:38:56 -0700

    test

---
+++ notebooks/Python Version.py
@@ -0,0 +1,46 @@
+# Databricks notebook source exported at Tue, 21 Jul 2015 17:38:56 UTC
+import pandas
+import scipy
+import numpy 
+pandas.__version__
+
+# COMMAND ----------
+
+"/home/ubuntu/databricks/python/bin/pip install --upgrade pandas" !!
+
+# COMMAND ----------
+
+os.system("/home/ubuntu/databricks/python/bin/pip install --upgrade pandas")
+
+# COMMAND ----------
+
+import numpy
+numpy.__version__
+
+# COMMAND ----------
+
+      install_requires=['boto', 'ujson', 'requests', 'protobuf', 'functools32', 'py4j', 'pandas>=0.15.2', 'numpy>=1.9.2', 'telemetry-tools'])
+
+
+# COMMAND ----------
+
+protobuf, functools32, py4j, ujson, telemetry-tools
+pandas>=0.15.2', 'numpy>=1.9.2',
+
+# COMMAND ----------
+
+import os
+os.system('/home/ubuntu/databricks/python/bin/pip install --upgrade pandas==0.15.2')
+
+# COMMAND ----------
+
+import pandas
+pandas.__version__
+
+# COMMAND ----------
+
+import os
+os.system(echo h)
+
+# COMMAND ----------
+
